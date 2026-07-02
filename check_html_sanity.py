f182a7698ae0ce78c36c28398a7c47a89a978ea6
Gregory Hong <hong0355945@gmail.com>
2026-07-02 14:27:17 +0800

    chore: clean up 37 one-off patching and utility scripts

--- check_html_sanity.py
+++
@@ -1,8 +0,0 @@
-﻿with open('content/KSSR_Syllabus/Primary6/English/Revision1/index.html', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-print("File size:", len(content))
-print("Number of script tags:", content.count('<script'))
-print("Number of button tags:", content.count('<button'))
-print("Contains START!:", "START!" in content)
-print("Contains startGame():", "startGame()" in content)
